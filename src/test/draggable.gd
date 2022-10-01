6adb21a4ef4bbfa71e4fd1bb486b524fde76596d
hulien22 <hejulien22@gmail.com>
2022-09-30 18:24:35 -0700

    test

---
+++ src/test/draggable.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+var selected = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("LEFT_CLICK"):
+		selected = true
+		
+func _physics_process(delta):
+	if selected:
+		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.pressed:
+			selected = false
+			var shortest_dist = 75
+#			for child in rest_nodes:
+#				var distance = global_position.distance_to(child.global_position)
+#				if distance < shortest_dist:
+#					child.select()
+#					rest_point = child.global_position
+#					shortest_dist = distance
