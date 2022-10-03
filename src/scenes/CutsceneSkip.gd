c4f300b5df0ec9d43e6e7766df15aaaa84a1b48a
hulien22 <hejulien22@gmail.com>
2022-10-03 14:10:33 -0700

    fix click through

--- src/scenes/CutsceneSkip.gd
+++ src/scenes/CutsceneSkip.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
 func _input_event(viewport, event, shape_idx):
-	print("here")
 	if event is InputEventMouseButton and event.pressed:
-		$AnimatedSprite.frame += 1
+		if $AnimatedSprite.frame >= 4:
+			$AnimatedSprite.stop()
+			$AnimatedSprite._on_animation_finished()
+		else:
+			$AnimatedSprite.frame += 1
+
--- src/scenes/game.gd
+++ src/scenes/game.gd
@@ -5,6 +5,7 @@ var click_all = false
 var ignore_unclickable = true
 var cur_click_order = 0
 var selected_obj
+var has_obj:bool = false
 var potion_recipes
 var potions
 var rng 
@@ -49,6 +50,7 @@ func _input(event):
 					shape["collider"].set_click_order(cur_click_order)
 					shape["collider"].pickup()
 					selected_obj = shape["collider"]
+					has_obj = true
 
 					if !click_all and ignore_unclickable:
 						break # Thus clicks only the topmost clickable
@@ -62,12 +64,13 @@ func _input(event):
 					instance.set_click_order(cur_click_order)
 					instance.pickup()
 					selected_obj = instance
+					has_obj = true
 					break
 
 			if !click_all and !ignore_unclickable:
 				break # Thus stops on the first shape
 	elif event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.pressed:
-		if selected_obj and selected_obj.has_method("drop"):
+		if has_obj and selected_obj and selected_obj.has_method("drop"):
 #			Check for stations
 			var shapes = get_world_2d().direct_space_state.intersect_point(get_global_mouse_position(), 32, [], 0x7FFFFFFF, true, true)
 			shapes.sort_custom(MyCustomSorter, "sort_ascending")
@@ -80,6 +83,7 @@ func _input(event):
 							selected_obj.queue_free()
 						break
 			selected_obj = null
+			has_obj = false
 	
 	if event is InputEventMouseButton and event.pressed and event.button_index == 2: # right mouse click
 		# The last 'true' enables Area2D intersections, previous four values are all defaults
@@ -95,12 +99,12 @@ func _input(event):
 func set_cur_click_order(co):
 	cur_click_order = co
 
-func _notification(event):
-	match event:
-		NOTIFICATION_WM_MOUSE_EXIT:
-			if selected_obj and selected_obj.has_method("drop"):
-				selected_obj.drop()
-				selected_obj = null
+#func _notification(event):
+#	match event:
+#		NOTIFICATION_WM_MOUSE_EXIT:
+#			if selected_obj and selected_obj.has_method("drop"):
+#				selected_obj.drop()
+#				selected_obj = null
 
 
 
